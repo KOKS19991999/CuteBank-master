9553f77e18619e54803f8f856c33222a749e61af
KOKS19991999 <yura.beliavtsev@yandex.ru>
2020-12-06 00:52:40 +0300

    Admin page

--- db/migrate/20201027100409_add_admin_user.rb
+++ db/migrate/20201027100409_add_admin_user.rb
@@ -2,6 +2,6 @@
 
 class AddAdminUser < ActiveRecord::Migration[6.0]
   def change
-    User.create user_is_admin: true, email: 'admin@admin.com'
+    User.create user_is_admin: true, email: 'admin@admin.com', :password => "123456"
   end
 end
